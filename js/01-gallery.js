c2e2c9ca45d7509b6fc4d4ba58d4fae41ed8e305
VladimirHavruniak <havruniak2@gmail.com>
2022-05-26 16:28:19 +0300

    partOfWork

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const wrapGallaryEl = document.querySelector(".gallery")
+const gallerylist = document.createElement("div");
+gallerylist.classList.add("gallery__item");
+
+galleryItems.map((el) => {
+    const galleryLink = document.createElement("a");
+    galleryLink.classList.add("gallery__link");
+    // galleryLink.href = el.original;
+    const gallaryImage = document.createElement("img");
+    gallaryImage.src = el.preview;
+    gallaryImage.alt = el.description;
+    gallaryImage.setAttribute("data-source", `${el.original}`);
+    gallaryImage.classList.add("gallery__image");
+    galleryLink.append(gallaryImage);
+    gallerylist.append(galleryLink);
+}
+);
+wrapGallaryEl.append(gallerylist);
+
+
+wrapGallaryEl.addEventListener("click", toOpenModalImage);
+
+function toOpenModalImage(event)  {
+    if (!event.target.classList.contains("gallery__image")) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+        ${event.target}
+`);
+    console.log(event.target);   
+    instance.show();
+};
+wrapGallaryEl.addEventListener("click", toOpenModalImage);
+
+
+
+
+
+
+
+
+
+
+
+// console.log(gallerylist);
+// const instance = basicLightbox.create(`
+//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="1280" height="auto">
+// `);
+
+// instance.show()
