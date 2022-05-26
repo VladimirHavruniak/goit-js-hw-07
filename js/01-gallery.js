4f4f4671a03b510a77abd3dd548038fd39613e0a
VladimirHavruniak <havruniak2@gmail.com>
2022-05-26 20:42:43 +0300

    firstExcercise

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,54 +1,40 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
-const wrapGallaryEl = document.querySelector(".gallery")
-const gallerylist = document.createElement("div");
-gallerylist.classList.add("gallery__item");
+const wrapGallaryEl = document.querySelector(".gallery");
 
-galleryItems.map((el) => {
-    const galleryLink = document.createElement("a");
-    galleryLink.classList.add("gallery__link");
-    // galleryLink.href = el.original;
-    const gallaryImage = document.createElement("img");
-    gallaryImage.src = el.preview;
-    gallaryImage.alt = el.description;
-    gallaryImage.setAttribute("data-source", `${el.original}`);
-    gallaryImage.classList.add("gallery__image");
-    galleryLink.append(gallaryImage);
-    gallerylist.append(galleryLink);
-}
-);
-wrapGallaryEl.append(gallerylist);
+const allGallaryItems = galleryItems.map((el) => {
+  const gallerylist = document.createElement("div");
+  gallerylist.classList.add("gallery__item");
 
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+  const gallaryImage = document.createElement("img");
+  gallaryImage.src = el.preview;
+  gallaryImage.alt = el.description;
+  gallaryImage.setAttribute("data-source", `${el.original}`);
+  gallaryImage.classList.add("gallery__image");
+  galleryLink.append(gallaryImage);
+  gallerylist.append(galleryLink);
+  return gallerylist;
+});
+wrapGallaryEl.append(...allGallaryItems);
 
-wrapGallaryEl.addEventListener("click", toOpenModalImage);
-
-function toOpenModalImage(event)  {
-    if (!event.target.classList.contains("gallery__image")) {
-        return;
+function toOpenModalImage(event) {
+        const gallaryImg = event.target;
+    event.preventDefault();
+    if (!gallaryImg.classList.contains("gallery__image")) {
+      return;
     }
-    const instance = basicLightbox.create(`
-        ${event.target}
-`);
-    console.log(event.target);   
-    instance.show();
-};
+  gallaryImg.onclick = () => {
+
+    basicLightbox.create(`<img src="${gallaryImg.dataset.source}">`).show();
+  };
+  // basicLightbox.create(`${gallaryImg}`).show();
+//   console.log(gallaryImg.description);
+//   console.log(event.target);
+}
 wrapGallaryEl.addEventListener("click", toOpenModalImage);
 
-
-
-
-
-
-
-
-
-
-
-// console.log(gallerylist);
-// const instance = basicLightbox.create(`
-//     <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="1280" height="auto">
-// `);
-
-// instance.show()
